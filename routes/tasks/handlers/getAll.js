3a3bb2279c1072d0796c67d9076db3fa14ae2fb3
JuanMa Garrido <JuanMa.Garrido@gmail.com>
2017-03-11 19:43:54 +0100

    logs server

--- routes/tasks/handlers/getAll.js
+++ routes/tasks/handlers/getAll.js
@@ -7,10 +7,13 @@ module.exports = (req,res) => {
 
   Task.find()
     .then( dbTasks => {
+      console.log("step1...")
       const userTasks = dbTasks.filter( task => task.userId === user.id )
+      console.log("step2...")
       const orderedTasks = _.orderBy(userTasks, task => {
         return [ task.status.category, task.status.order ]
       }, ['asc', 'asc']);
+      console.log("step3...")
       const tasks = _.groupBy(orderedTasks, 'status.category');
       console.log(tasks)
       res.render('tasks/list', { tasks, user })
